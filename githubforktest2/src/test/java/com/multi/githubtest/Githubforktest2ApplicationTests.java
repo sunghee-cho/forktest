0e7679ce30b260519010733abf2461dfd0bf0870
이현용 <kqzx001@naver.com>
2021-04-27 14:10:21 +0900

    첫번째 포크테스트2

---
+++ githubforktest2/src/test/java/com/multi/githubtest/Githubforktest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.githubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Githubforktest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
